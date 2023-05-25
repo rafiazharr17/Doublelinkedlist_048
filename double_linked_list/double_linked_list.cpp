c666a4aad63d82c273b0e835ca260d5181949ea7
rafiazharr17 <rafipilo17@gmail.com>
2023-05-25 10:59:21 +0700

    membuat node current menjadi start

--- double_linked_list/double_linked_list.cpp
+++ double_linked_list/double_linked_list.cpp
@@ -45,7 +45,17 @@ void DoubleLinkedList::addNode() {
 
 	if (START == NULL || nim <= START->noMhs) {
 		if (START != NULL && nim == START->noMhs) {
-			
+			cout << "\nDuplicate number not allowed" << endl;
+			return;
 		}
+		newNode->next = START;
+		if (START != NULL)
+			START->prev = newNode;
+		newNode->prev = NULL;
+		START = newNode;
+		return;
 	}
+
+	node* current = START;
+	
 }
